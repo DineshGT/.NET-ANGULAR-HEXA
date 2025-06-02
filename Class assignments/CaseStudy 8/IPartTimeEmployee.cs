f3fda9ec46832ea564599ecb1c66bf58a4fe0bf0
DineshGT <dineshgreet2004@gmail.com>
2025-06-02 17:07:04 +0530

    case study 8

---
+++ Class assignments/CaseStudy 8/IPartTimeEmployee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsCaseStudy
+{
+    public interface IPartTimeEmployee : IEmployee<Employee>
+    {
+
+        double Basic { get; set; }
+        double Da { get; set; }
+        double NetSalary { get; set; }
+
+        //method
+
+        double CalculateSalary();
+    }
+
+
+}
